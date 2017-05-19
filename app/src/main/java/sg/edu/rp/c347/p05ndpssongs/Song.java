c3705ab47b28d4e2380cce497efd288f447e7ecd
15017612 <15017612@myrp.edu.sg>
2017-05-19 14:57:42 +0800

    song class

--- app/src/main/java/sg/edu/rp/c347/p05ndpssongs/Song.java
+++ app/src/main/java/sg/edu/rp/c347/p05ndpssongs/Song.java
@@ -5,4 +5,42 @@ package sg.edu.rp.c347.p05ndpssongs;
  */
 
 public class Song {
+
+    private int _id;
+    private String title;
+    private String singer;
+    private int year;
+    private int stars;
+
+
+    public Song (String title, String singer, int year, int stars){
+        this.title = title;
+        this.singer = singer;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int get_id(){
+        return _id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getSinger(){
+        return singer;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public int getStars(){
+        return stars;
+    }
+
+
+
 }
+
